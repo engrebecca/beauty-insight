42e8c5cf1cef03b4a1f39dfc22a2e258cdbd9139
reng <reng412@gmail.com>
2020-12-28 15:17:50 -0800

    Add rating and reviews to axios request

--- server.js
+++ server.js
@@ -18,13 +18,19 @@ axios.get("https://www.sephora.com/shop/foundation-makeup?ref=100082,100058,1457
     $(".css-12egk0t").each((i, element) => {
         let title = $(element).find("a").attr("aria-label");
         let brand = $(element).find("span[data-at*= 'sku_item_brand']").text();
+        let msrp = $(element).find("span[data-at*= 'sku_item_price_list']").text();
+        let rating = $(element).find("div[class= 'css-jp4jy6']").attr("aria-label");
+        let reviews = $(element).find("span[class= 'css-1dk1ux']").text();
         let link = `https://sephora.com${$(element).find("a").attr("href")}`;
         let image = `https://sephora.com${$(element).find("img").attr("src")}`;
         results.push({
             title: title,
             brand: brand,
+            msrp: msrp,
             link: link,
-            image: image
+            image: image,
+            rating: rating,
+            reviews: reviews
         })
     })
     console.log(results);
