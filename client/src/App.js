daf59de8a901c63251b15d51cddf77c06357951e
reng <reng412@gmail.com>
2021-01-12 15:48:14 -0800

    Remove navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
-import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Jumbotron />
       <Homepage />
     </div>
