cb3bdfdb4dd8100a8bc6a7de67695a72626919c3
reng <reng412@gmail.com>
2021-01-13 15:40:01 -0800

    Add comments to frontend routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
 export default {
+    // Get all products from database
     getProducts: function () {
         return axios.get("/products");
     },
+    // Scrape a webpage and send to database
     addProducts: function (url) {
         return axios.post("/scrape", url);
     },
+    // Delete one product
     deleteProduct: function (id) {
         return axios.delete(`/products/delete/${id}`);
     },
+    // Delete all products
     deleteAll: function () {
         return axios.delete("/products/delete");
     },
+    // Get average MSRP for all products
     getAverage: function () {
         return axios.get("/products/average")
     }
